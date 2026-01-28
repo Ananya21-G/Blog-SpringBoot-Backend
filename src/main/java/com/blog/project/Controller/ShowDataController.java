550453185be232a9662e7648c5d20fc2f835cc0f
Ananya21-G <ananyaghosh.2106@gmail.com>
2026-01-28 15:04:27 +0530

    routed GetMapping

--- src/main/java/com/blog/project/Controller/ShowDataController.java
+++ src/main/java/com/blog/project/Controller/ShowDataController.java
@@ -1,4 +1,23 @@
 package com.blog.project.Controller;
 
+import com.blog.project.Entity.BlogEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.blog.project.Repository.BlogRepository;
+import java.util.List;
+
+@RestController
+@RequestMapping("/blog")
 public class ShowDataController {
+    private final BlogRepository blogRepository;
+
+    public ShowDataController(BlogRepository blogRepository){
+        this.blogRepository = blogRepository;
+    }
+
+    @GetMapping
+    public List<BlogEntity> GetData(){
+    return blogRepository.findAll();
+    }
 }
