18d209398ddb180e131f22c9f9307fc948ea8f6c
Ananya21-G <ananyaghosh.2106@gmail.com>
2026-01-28 14:40:49 +0530

    created BlogRepository

--- src/main/java/com/blog/project/Repository/BlogRepository.java
+++ src/main/java/com/blog/project/Repository/BlogRepository.java
@@ -1,5 +1,7 @@
 package com.blog.project.Repository;
 
-public class BlogRepository {
+import com.blog.project.Entity.BlogEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface BlogRepository extends JpaRepository<BlogEntity, String>{
 }
