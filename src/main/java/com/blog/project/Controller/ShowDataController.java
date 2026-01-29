4b993ddb6bfb21ac2836205bf19aede8cccc53d8
Ananya21-G <ananyaghosh.2106@gmail.com>
2026-01-29 15:22:23 +0530

    Fix path variable mapping and handle missing blog IDs safely

--- src/main/java/com/blog/project/Controller/ShowDataController.java
+++ src/main/java/com/blog/project/Controller/ShowDataController.java
@@ -2,10 +2,12 @@ package com.blog.project.Controller;
 
 import com.blog.project.Entity.BlogEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.blog.project.Repository.BlogRepository;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/blog")
@@ -19,5 +21,12 @@ public class ShowDataController {
     @GetMapping
     public List<BlogEntity> GetData(){
     return blogRepository.findAll();
+    }
+
+    @GetMapping("/{blogid}")
+    public BlogEntity GetIdData(@PathVariable UUID blogid){
+
+        BlogEntity blog = blogRepository.findById(blogid).orElseThrow(()-> new RuntimeException("Blog not found"));
+        return blog;
     }
 }
